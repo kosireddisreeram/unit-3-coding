0b9abf9254a3cccc97a62aa189c2c9add0e68bc6
kosireddisreeram <78956214+kosireddisreeram@users.noreply.github.com>
2022-03-04 15:32:18 +0530

    notes

---
+++ day-3/notes.js
@@ -0,0 +1,28 @@
+// // // to create a object
+// // Object.create
+
+// const tiger = object.create(cat);
+// tiger.name='tiger';
+// console.log("tiger",tiger.name)
+
+const masai_student={
+    batch:"web 16",
+    institute:"masaie",
+    current_unit:3,
+    nickname:'pikachu'
+}
+
+const s1=Object.create(masai_student,{name:{value:"anupam"}});
+console.log(s1); // output will be anupam 
+//      another way to create give value n
+
+
+// ecommerece products catalog . 
+
+const ecommerece={
+    batch:"web 16",
+    institute:"masaie",
+    current_unit:3,
+    nickname:'pikachu'
+}
+
